9a67f3c65b320827e06dbaa8dcbc71dc29cb8a89
Barbara Guzman <barbara@172-29-210-45.dynapool.nyu.edu>
2019-09-28 23:20:29 -0400

    called giphy api, recieved response object back, passed it to html template

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
+import urllib, json
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    data=json.loads(urllib.request.urlopen("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOURAPIKEYHERE&limit=1").read())
+    print(json.dumps(data, sort_keys=True, indent=4))
+    giphys = json.dumps(data, sort_keys=True, indent=4)
+
+    return render_template('index.html', giphys=giphys)
 
 if __name__ == "__main__":
     app.run(debug=True)
